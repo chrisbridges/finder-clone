3dd2cc71a2365aad88265c62523ee660a0714f0c
chris.bridges <christopher_bridges@icloud.com>
2018-07-05 19:03:28 -0400

    basic styling

--- tagpicker/src/TagPicker.js
+++ tagpicker/src/TagPicker.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './TagPicker.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 
 export class TagPicker extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // currentParentID: null,
-      // currentParentName: null,
       path: [{parentName: 'Root', parentID: null}]
     };
   }
@@ -19,11 +18,20 @@ export class TagPicker extends Component {
       return doc.parent === parentID;
     });
 
+    // Icons
+    const folderIcon = <i className="fa fa-folder-open" aria-hidden="true"></i>;
+    const rightChevron = <i className="fa fa-chevron-right" aria-hidden="true"></i>;
+    const check = <i className="fa fa-check-square"></i>;
+
     return docsToDisplay.map(doc => {
       if (doc.isFolder) {
-        return <li key={doc._id} onClick={() => this.setState({path: [...this.state.path, {parentName: doc.name, parentID: doc._id}]})}>{doc.name}</li>;
+        return <li className="doc folder" key={doc._id} onClick={() => this.setState({path: [...this.state.path, {parentName: doc.name, parentID: doc._id}]})}><span className="doc-name">{folderIcon}{doc.name}</span><span className="right-chevron">{rightChevron}</span></li>;
       } else {
-        return <li key={doc._id} onClick={() => this.addOrRemoveSelectedTag(doc._id)}>{doc.name}</li>;
+        // if tag is selected, add checkbox
+        if (this.props.selectedTags.indexOf(doc._id) === -1) {
+          return <li className="doc tag" key={doc._id} onClick={() => this.addOrRemoveSelectedTag(doc._id)}><span className="doc-name">{doc.name}</span></li>;
+        }
+        return <li className="doc tag" key={doc._id} onClick={() => this.addOrRemoveSelectedTag(doc._id)}><span className="doc-name">{check}{doc.name}</span></li>; 
       }
     });
   }
@@ -40,30 +48,6 @@ export class TagPicker extends Component {
   }
 
   goBackOneParent () {
-    // find doc of our current parent
-    // const currentParentDoc = this.props.tags.find(doc => {
-    //   return doc._id === this.state.currentParentID;
-    // });
-    // // find that doc's parent's ID
-    // const parentOfCurrentParentID = currentParentDoc.parent;
-
-    // let parentOfCurrentParentName;
-    // // if parent has no ID, name = null
-    // if (parentOfCurrentParentID === null) {
-    //   parentOfCurrentParentName = null;
-    // } else {
-    //   // else, find that parent's doc, and then it's name
-    //   const parentOfCurrentParentDoc = this.props.tags.find(doc => {
-    //     return parentOfCurrentParentID === doc._id;
-    //   });
-    //   parentOfCurrentParentName = parentOfCurrentParentDoc.name;
-    // }
-
-    // this.setState({currentParentID: parentOfCurrentParentID, currentParentName: parentOfCurrentParentName});
-    // I'm sure there's a more efficient, or at least a more clear way of going about this
-    // but I believe the structure of our data necessitates something similar to what I have here -   
-    // from our current parent, use its parent value to find that parent's doc, then display the name value (unless it's null)
-
     // creating copy of path to not directly alter state
     const copyOfPath = this.state.path;
     copyOfPath.pop();
@@ -74,9 +58,9 @@ export class TagPicker extends Component {
     const mostRecentPath = this.state.path[this.state.path.length - 1];
     return (
     <div className="tag-picker">
-    <h2>{mostRecentPath.parentName}</h2>
-      {mostRecentPath.parentID === null ? null : <button onClick={() => this.goBackOneParent()}>Back</button>}
-      <ul>{this.displayDocs()}</ul>
+      <h2 className="folder-name">{mostRecentPath.parentName}</h2>
+      {mostRecentPath.parentID === null ? null : <button onClick={() => this.goBackOneParent()}>&lt; Back</button>}
+      <ul className="docs-list">{this.displayDocs()}</ul>
     </div>
     )
   }
