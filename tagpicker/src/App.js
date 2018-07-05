e2302b22a8a512d1cb137e4f3175e33277b148e4
chris.bridges <christopher_bridges@icloud.com>
2018-07-05 14:37:58 -0400

    folder child traversal functional

--- tagpicker/src/App.js
+++ tagpicker/src/App.js
@@ -11,25 +11,10 @@ class App extends Component {
     };
   }
 
-  addOrRemoveSelectedTag (tag) {
-    const indexOfTag = this.state.selectedTags.indexOf(tag);
-    console.log(indexOfTag);
-    if (indexOfTag === -1) {
-      this.setState({selectedTags: [...this.state.selectedTags, tag]});
-    } else {
-      // creating copy of state.selectedTags to avoid mutating state directly
-      const copyOfSelectedTags = [...this.state.selectedTags];
-      copyOfSelectedTags.splice(indexOfTag, 1);
-      this.setState({selectedTags: copyOfSelectedTags});
-    }
-    console.log(this.state.selectedTags);
-  }
-
-
   render() {
     return (
       <div className="App">
-        <TagPicker tags={tags} selectedTags={this.state.selectedTags} onTagSelectionChange={selectedTag => this.addOrRemoveSelectedTag(selectedTag)}/>
+        <TagPicker tags={tags} selectedTags={this.state.selectedTags} onTagSelectionChange={selectedTags => this.setState({selectedTags})} />
       </div>
     );
   }
--- tagpicker/src/TagPicker.js
+++ tagpicker/src/TagPicker.js
@@ -1,15 +1,63 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function TagPicker(props) {
+export class TagPicker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentParent: null
+    };
+  }
 
-  const displayTags = props.tags.map(tag => {
-    return <li key={tag._id} onClick={() => props.onTagSelectionChange(tag._id)}>{tag.name}</li>
-  });
+  displayTags(tagsToDisplay=this.props.tags) {
+    // take all tags, filter them by proper parent ID
+      // map those
+    const parentID = this.state.currentParent;
+    const tags = tagsToDisplay.filter(tag => {
+      return tag.parent === parentID;
+    });
 
-  return (
+    return tags.map(tag => {
+      return <li key={tag._id} onClick={() => this.folderOrTag(tag.isFolder, tag._id)}>{tag.name}</li>;
+    });
+  }
+
+  folderOrTag (folder, tagID) {
+    if (folder) {
+      this.findFolderChildren(tagID);
+    } else {
+      this.addOrRemoveSelectedTag(tagID);
+    }
+  }
+
+  findFolderChildren (tagID) {
+    // display all tags that have parent of folder ID
+    const folderChildren = this.props.tags.filter(tag => {
+      return tag.parent === tagID;
+    });
+    console.log(folderChildren);
+    this.setState({currentParent: tagID});
+    this.displayTags(folderChildren);
+  }
+
+  addOrRemoveSelectedTag (tag) {
+    // for checking tags
+    let selectedTags = [...this.props.selectedTags];
+    const indexOfTag = selectedTags.indexOf(tag);
+    if (indexOfTag === -1) {
+      selectedTags = [...selectedTags, tag];
+    } else {
+      selectedTags.splice(indexOfTag, 1);
+    }
+    this.props.onTagSelectionChange(selectedTags);
+  }
+
+  render() {
+    return (
     <div className="tag-picker">
-      <ul>{displayTags}</ul>
+      <ul>{this.displayTags()}</ul>
     </div>
-  );
+    )
+  }
 }
 
+export default TagPicker;
